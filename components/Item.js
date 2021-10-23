10e6a0c15464b6e514db3e126fc1269635ded428
Tiger <tiger.chalernphon15@gmail.com>
2021-10-24 10:44:34 +1100

    add delete button, install async storage

--- components/Item.js
+++ components/Item.js
@@ -3,9 +3,11 @@ import { View, Text, StyleSheet, Button, Image, TouchableOpacity } from 'react-n
 
  
 export const ItemList = (props) => {
+
     return (
       <View style= {styles.container}>
-        <Text>{props.text}</Text>
+        <Text style= {styles.title}>{props.text}</Text>
+        <Button title="Delete" onPress={() => props.delete(props.id)}/>
       </View>
     )
   }
@@ -13,8 +15,20 @@ export const ItemList = (props) => {
 const  styles = StyleSheet.create ({
     container: {
         padding: 10,
-        backgroundColor: 'lightgreen',
-        borderBottomColor: 'black',
-        borderBottomWidth: 1,
+        backgroundColor: '#EBF2FD',
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10,
+        padding: 30,
+        borderRadius: 15,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        flex: 1,
     },
 })
