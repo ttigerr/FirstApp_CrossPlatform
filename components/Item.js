fb0c038204fd6ac63a8d63d8cea1e70603632edd
Tiger <tiger.chalernphon15@gmail.com>
2021-10-24 23:22:18 +1100

    Add icons

--- components/Item.js
+++ components/Item.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button, Image, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Button, Image, TouchableOpacity, CheckBox } from 'react-native'
+
 
  
 export const ItemList = (props) => {
 
     return (
       <View style={(props.status) ? styles.done : styles.container}>
+        <TouchableOpacity 
+            style={styles.checkList} 
+            onPress={() => props.done(props.id)}>
+            <Image source={ require('../assets/circle_check.png') } style={styles.icon} />
+
+        </TouchableOpacity>
         <Text style= {(props.status) ? styles.textDone : styles.title}>{props.text}</Text>
-        <Button title="Delete" onPress={() => props.delete(props.id)}/>
-        <Button title="Done" onPress={() => props.done(props.id)}/>
+        {/* <Button title="Delete" onPress={() => props.delete(props.id)}/> */}
+        <TouchableOpacity 
+            onPress={ () => props.delete(props.id) } >
+          <Image source={ require('../assets/trash_icon.png') } style={styles.icon} />
+        </TouchableOpacity>
+        
       </View>
     )
   }
@@ -32,6 +43,13 @@ const  styles = StyleSheet.create ({
         fontWeight: 'bold',
         flex: 1,
     },
+    checkList: {
+        width: 24,
+        height: 24,
+        opacity: 0.4,
+        marginRight: 15,
+        borderRadius: 5,
+    },
     textDone: {
         flex: 1,
         color: "#298133",
@@ -51,4 +69,8 @@ const  styles = StyleSheet.create ({
         alignItems: "center",
 
     },
+    icon: {
+        width: 24,
+        height: 24,
+    }
 })
